c0748d26bbfac87fbe1177078dfd630565bc812e
Antor4q <tariquelislam2015@gmail.com>
2024-02-14 11:54:41 +0600

    finished today work

---
+++ scripts/utility.js
@@ -0,0 +1,46 @@
+function hideElementById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.add('hidden');
+}
+
+function showElementById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.remove('hidden');
+}
+
+function setBackgroundColorById (elementID){
+    const element = document.getElementById(elementID);
+    element.classList.add('bg-[#FFA500]');
+}
+
+function removeBackgroundColorById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.remove('bg-[#FFA500]');
+}
+
+function getARandomAlphabet(){
+    const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
+    const alphabets = alphabetString.split('');
+    
+    const randomNumber = Math.random()*25;
+    const index = Math.round(randomNumber);
+
+    const alphabet = alphabets[index];
+    return alphabet;
+}
+
+// function getARandomAlphabet(){
+//     // get or create an alphabet array
+//     const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
+//     const alphabets = alphabetString.split('');
+//     // console.log(alphabets);
+
+//     // get a random index between 0 - 25
+//     const randomNumber = Math.random()*25;
+//     const index = Math.round(randomNumber);
+   
+
+//     const alphabet = alphabets[index];
+//     // console.log(index, alphabet);
+//     return alphabet;
+// }
